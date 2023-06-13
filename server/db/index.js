06fcbf9b90330965b398c00e21a47c22b778fc9c
em2233 <emilypaye@gmail.com>
2023-06-13 16:29:33 -0400

    added Order model, OrderItem model, and updated index.js with new model info, also added associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,31 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+
+const Item = require("./models/Item");
+
+const Order = require("./models/Order");
+
+const OrderItem = require("./models/OrderItem");
 
 //associations could go here!
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Order.hasMany(OrderItem);
+OrderItem.belongsTo(Order);
+
+Item.hasMany(OrderItem);
+OrderItem.belongsTo(Item);
 
 module.exports = {
   db,
   models: {
     User,
+    Item,
+    Order,
+    OrderItem,
   },
-}
+};
